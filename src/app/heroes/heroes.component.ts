50494ecefd64be3cb30d574f0ae9e89db44fa47f
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2020-11-19 13:49:47 +0900

    コンストラクタに引数追加

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,12 +8,14 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+
   heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
