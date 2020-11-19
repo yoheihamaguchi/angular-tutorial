d70318054462bc73ee168ca03c05c9f18b83873b
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2020-11-19 12:34:36 +0900

    サービスを受け取る処理追加

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +8,17 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstrom',
-  // };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  constructor() {}
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit(): void {}
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
 }
